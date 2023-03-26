597103c97645e5235e7f9a9aede0a48c1bbba868
Ibrahim Aji <ibrahim.aji@live.com>
2023-03-27 06:05:03 +0700

    Edit register

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -45,5 +45,8 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::loginView(function(){
             return view('auth.login');
         });
+        Fortify::registerView(function(){
+            return view('auth.register');
+        });
     }
 }
